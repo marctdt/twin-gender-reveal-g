7ee37a53c62890aab12ec7767936a843799efbcf
Marc Tran Du Trieu <38071131+marctdt@users.noreply.github.com>
2026-02-01 12:47:47 +0000

    Add react-router-dom and update routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "@github/spark/spark"
 
 import App from './App.tsx'
+import LeaderboardPage from './components/LeaderboardPage.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
 import "./main.css"
@@ -11,6 +13,11 @@ import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+      </Routes>
+    </BrowserRouter>
    </ErrorBoundary>
 )
